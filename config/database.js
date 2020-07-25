149873872e0e5adffc9a5ca9c085c29cf6f650f3
Bao <lbx01072004@gmail.com>
2020-07-25 14:38:25 +1000

    git push heroku master

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // DB Config
 const mongoose = require("mongoose");
 
-const MONGODB_URI = ""
-const mongoURI = "mongodb://localhost/pro3";
+const MONGODB_URI = process.env.MONGODB_URI;
+const mongoURI = "mongodb://admin:secret1@ds059165.mlab.com:59165/heroku_9r0p3wvn"
 
 function connectDb(){
 
