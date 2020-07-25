27a9195dc2f58e6bce5f0f2ffd4e1b082d71dbd7
Bao <lbx01072004@gmail.com>
2020-07-25 14:50:37 +1000

    .

--- client/src/components/UserFunctions/userFunctions.js
+++ client/src/components/UserFunctions/userFunctions.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-let serverUrl = "http://localhost:5001"
-// (change serverUrl on heroku)
-if (process.env.NODE_ENV === "production") {
-    serverUrl = 'https://afternoon-coast-88401.herokuapp.com/';
-}
+// let serverUrl = "http://localhost:5001"
+// // (change serverUrl on heroku)
+// if (process.env.NODE_ENV === "production") {
+//     serverUrl = 'https://afternoon-coast-88401.herokuapp.com/';
+// }
 
 export const registerUser = userData => {
     // console.log(userData);
     return axios
-    .post(serverUrl + '/api/register', {
+    .post('/api/register', {
         first_name: userData.first_name,
         last_name: userData.last_name,
         email: userData.email,
@@ -23,7 +23,7 @@ export const registerUser = userData => {
 export const loginUser = userData => {
     // console.log(userData);
     return axios
-    .post(serverUrl + '/api/login', {
+    .post('/api/login', {
         email: userData.email,
         password: userData.password
     })
@@ -39,7 +39,8 @@ export const loginUser = userData => {
 
 export const getUsers = userData => {
     return axios
-    .get(serverUrl + '/api/displayusers', {
+    .get(
+        '/api/displayusers', {
     })
     .then(response => {
         // console.log(response.data);
