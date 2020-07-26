248b6ec0d1db14958d66cfdd10b289f86f508521
Unknown <hc.ngu01@gmail.com>
2020-07-26 22:20:28 +0800

    login route working

--- models/User.js
+++ models/User.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
-  // user_id: {
-  //   type: Schema.Types.ObjectId,
-  //   required: true,
-  //   ref: "User"
-  // },
   first_name: {
     type: String,
     required: true
@@ -25,6 +21,49 @@ const UserSchema = new Schema({
     required: true
   }
 }, {timestamps: true});
+
+
+/**
+ * Password hash middleware.
+ */
+UserSchema.pre("save", async function save(next) {
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    // login
+    // enter passsword
+    // bcrypt => add salt to entered password
+    // encrypt
+    // compare if same as db record
+
+    await bcrypt
+        .genSalt(10)
+        .then((salt) => {
+            return bcrypt.hash(user.password, salt);
+        })
+        .then((hash) => {
+            user.password = hash;
+            next();
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+UserSchema.methods.comparePassword = function comparePassword(
+    candidatePassword,
+    cb
+) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        cb(err, isMatch);
+    });
+};
+
 
 const User = mongoose.model("users", UserSchema);
 
